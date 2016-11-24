02813e7250fac3bacfc18a0940426d7246b02e5b
Gvo3d <denis.yakimov89@gmail.com>
2016-11-24 21:13:14 +0200

    StringBuilder test for IRep project SQL statements

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	StringBuilder sb = new StringBuilder("Hello, ");
+        sb.append("motherfucker");
+        sb.append(" !!!");
+        System.out.println(sb);
+    }
+}
